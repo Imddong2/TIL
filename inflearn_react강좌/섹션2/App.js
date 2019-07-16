ede73936abee89a4518437070f4fa51a046a334e
Imddong <hjinny9822@gmail.com>
2019-07-16 18:29:33 +0900

    인프런 강좌_섹션2_react시작 (2/9)

---
+++ inflearn_react강좌/섹션2/App.js
@@ -0,0 +1,14 @@
+//리액트모듈을 불러와서 사용하겠다.
+//사용하기 위해서는 리액트 불러와야한다.
+import React, {Component} from 'react';
+
+//class를 이용하여 component만들기
+class App extends Component{
+    //render메소드-> jsx형태의 코드 리턴해줘야함
+    render(){
+        <div>
+            <h1>안녕하세요 리액트</h1>
+        </div>
+    };
+}
+export default App;
